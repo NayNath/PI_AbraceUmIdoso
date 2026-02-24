997eb8235927f94eedb6d25b45db0da0472174fd
nathaly cunha <nathy.zulmira@gmail.com>
2026-02-24 15:11:30 -0300

    add classe pessoas

---
+++ Classes/Pessoas.php
@@ -0,0 +1,22 @@
+<?php
+require_once "./conexao/conexao.php";
+class Pessoas{
+    public string $nomePessoa, $cpf, $fotoPerfil, $sobre;
+    public $dateNascimento;
+
+
+    public function __construct($nomePessoa, $cpf, $fotoPerfil, $sobre, $dateNascimento){
+        $this->nomePessoa = $nomePessoa;
+        $this->cpf = $cpf;
+        $this->fotoPerfil = $fotoPerfil;
+        $this->sobre = $sobre;
+        $this->dateNascimento = $dateNascimento;
+    }
+    public function __get($valor){
+        return $this->$valor;
+    }
+    public function __set($valor, $campo){
+        return  $this->$valor = $campo;
+    }
+}
+?>
