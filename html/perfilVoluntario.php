75bb7587c3fe0594561ce5b531f50c417402c1cb
nathaly cunha <nathy.zulmira@gmail.com>
2026-03-02 13:50:36 -0300

    mudanças na tela inicial e no perfil do voluntario

---
+++ html/perfilVoluntario.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil Voluntário</title>
+     <link rel="stylesheet" href="./../css/estilo.css">
+    <link rel="stylesheet" href="./../css/">
+
+</head>
+<body>
+        <head>
+    <meta charset="UTF-8">
+    <title>Seu Título</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
+   
+    <link rel="stylesheet" href="caminho/para/seu/style.css">
+    </head>
+    <header class="cabecalho">
+        <a href="#" class="logo">
+            <img src="./../imagem/logoPI.jpg" alt="LogoAbraceUmIdoso">
+        </a>
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="./cadastro.html">Cadastrar</a></li>
+                <li><a href="/login" class="ga-nav" title="agendamento">Login</a></li>
+            </ul>
+            <ul>
+                <li><a href="./inicio.html">Início</a></li>
+                <li><a href="/agendamento" class="ga-nav" title="agendamento">Agendamento</a></li>
+                <li><a href="/cartas" class="ga-nav" title="cartas">Cartas</a></li>
+                <li><a href="./contatos.html" class="ga-nav" title="contato">Fale Conosco</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <footer class="rodape"><p>© 2025 RastroCerto. Todos os direitos reservados</p></footer>
+</body>
+</html>
